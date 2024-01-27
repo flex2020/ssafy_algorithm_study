b48afbb1cd738e9604348f98d8f6418304c8169f
Jiwon0718 <wldnjs7851@naver.com>
2024-01-27 21:15:45 +0900

    feat: [SWEA]7964 부먹왕국의 차원 관문 풀이 추가

---
+++ 이지원/swea/SolutionSWEA7964.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class SolutionSWEA7964 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int D = Integer.parseInt(st.nextToken());
+
+            int[] map = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < map.length; i++) {
+                map[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int index = -1;
+            int result = 0;
+            aa: while (index < map.length - D) {
+                for (int i = D; i > 0; i--) {
+                    if (map[index + i] == 1) {
+                        index += i;
+                        continue aa;
+                    }
+                }
+                
+                index += D;
+                if (index < map.length && map[index] == 0) {
+                    result++;
+                }
+            }
+
+            System.out.println("#" + testCase + " " + result);
+        }
+    }
+}
