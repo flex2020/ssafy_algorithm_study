7a9954f234ff1d994e19011c1644da11cbe80d6a
jiwon718 <wldnjs7851@naver.com>
2024-02-04 15:34:25 +0900

    fix: [백준]24511 queuestack 풀이 수정

--- 이지원/boj/SolutionBaekJoon24511.java
+++ 이지원/boj/SolutionBaekJoon24511.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution24511 {
+public class SolutionBaekJoon24511 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
