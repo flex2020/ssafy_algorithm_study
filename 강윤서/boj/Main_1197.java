9abb50503b90cf873144b75752d1fa2f11e0dc2a
zpqmdh <ysys1130@gmail.com>
2024-04-04 01:10:18 +0900

    [BOJ] 1197 최소 스패닝 트리

--- 강윤서/boj/Main_1197.java
+++ 강윤서/boj/Main_1197.java
@@ -1,2 +1,39 @@
-package 강윤서.boj;public class Main_1197 {
+package 강윤서.boj;
+import java.io.*;
+import java.util.*;
+public class Main_1197 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        PriorityQueue<int[]> PQ = new PriorityQueue<>(Comparator.comparingInt(p -> p[1]));
+        List<List<int[]>> board = new ArrayList<>();
+        boolean[] visited = new boolean[V+1];
+        for (int i=0; i<=V; i++) {
+            board.add(new ArrayList<>());
+        }
+        for (int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            board.get(a).add(new int[] {b, c});
+            board.get(b).add(new int[] {a, c});
+        }
+        int answer = 0;
+        PQ.offer(new int[] {1, 0});
+        while (!PQ.isEmpty()) {
+            int[] cur = PQ.poll();
+            if (visited[cur[0]]) continue;
+            visited[cur[0]] = true;
+            answer += cur[1];
+            for (int[] next : board.get(cur[0])) {
+                if (!visited[next[0]]) {
+                    PQ.offer(next);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
 }
